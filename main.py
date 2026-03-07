fe44e18f63a2d6e49b7c6faa9c954327ad631d8d
Kyilee Bell <bellkyilee@gmail.com>
2026-03-07 11:43:06 -0600

    added the linked list data structure and some of the main program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 class Node:
     def __init__(self, key, value):
@@ -13,17 +14,51 @@ class LinkedList:
         self.tail = None
     
     def set(self, key, value):
-        pass
-    
-    def get(self):
-        pass
+        try:
 
+            new_node = Node(key, value)
+            if self.head is None:
+                self.head = new_node
+                self.tail = new_node
+
+            else:
+                self.tail.next = new_node
+                self.tail = new_node
+
+            with open('data.db', 'a') as f:
+                line = f"{new_node.key},{new_node.value}"
+                f.write(line)
+            
+            return
+
+        except IOError as e:
+            print(f"Error appending to data.db: {e}")
+    
+    def get(self, key):
+        current = self.head
+        while current:
+            if current.key == key:
+                print(current.key, current.value, '\n')
+            return
+        print(f"{key} not found.")
+             
 
 def main(): 
+    key_vals = LinkedList()
+    
     while (True):
         user_input = input()
-        if user_input == 'EXIT':
-            break
+        command = user_input.split(maxsplit=1)[0]
+        match command:
+            case 'SET':
+                pass
+            case 'GET':
+                parts = user_input.split()
+                key = int(parts[1])
+                key_vals.get(int(key))
+            case 'EXIT':
+                break
+
     
     return 0
 
